86c0a027ef4e00c053824d9dde211faa21bca147
Gonzalo Martinez <gonzalovam@hotmail.com>
2024-01-29 20:54:35 -0300

    peticion a api a continuar

--- js/cuota-api.js
+++ js/cuota-api.js
@@ -23,16 +23,46 @@ fetch(url)
     .then(data => {
         // Manipula los datos recibidos y renderiza la información en la página
         console.log(data)
-        renderizarInformacionPagos(data);
+        // renderizarInformacionPagos(data);
+        renderizarVistasMensuales(data)
     })
     .catch(error => {
         console.error('Error de fetch:', error);
     });
 
 // Función para renderizar la información de pagos
-function renderizarInformacionPagos(data) {
-    var informacionPagosDiv = document.getElementById('informacion-pagos');
+// function renderizarInformacionPagos(data) {
+//     var informacionPagosDiv = document.getElementById('informacion-pagos');
 
-    // Aquí puedes manipular los datos y renderizar la información en el DOM
-    // Por ejemplo, podrías crear elementos HTML como <p>, <ul>, etc., y agregarlos a informacionPagosDiv.
+//     // Aquí puedes manipular los datos y renderizar la información en el DOM
+//     // Por ejemplo, podrías crear elementos HTML como <p>, <ul>, etc., y agregarlos a informacionPagosDiv.
+// }
+
+function renderizarVistasMensuales(usuario) {
+    var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+    meses.forEach(function (mes) {
+        var vistaMes = document.getElementById(mes);
+
+        // Filtra los pagos correspondientes al mes actual
+        var pagosMes = usuario.payments.filter(function (pago) {
+            console.log("checkeando pago de" + pago.paymentDateTime)
+            var mesDePago = parseInt(pago.paymentDateTime.split('/')[1], 10);
+            console.log(mesDePago)
+            return usuario.activity === pago.activity && mesDePago - 1 === meses.indexOf(mes);
+        });
+
+        // Renderiza la información en la vista del mes
+        pagosMes.forEach(function (pago) {
+            var divPago = document.createElement("div");
+            divPago.textContent = "Pago: " + pago.paymentDateTime;
+
+            // Marca como completado si el pago tiene status "Completed"
+            if (pago.status === "Completed") {
+                divPago.classList.add("pago-completado");
+            }
+
+            vistaMes.appendChild(divPago);
+        });
+    });
 }
